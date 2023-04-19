21c4c697238f77257d536146feac331987839671
EduarVicen <123512047+EduarVicen@users.noreply.github.com>
2023-04-18 18:31:35 -0600

    ya esta 4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   //My RAILWAY 
    
   API_URI: 'https://nuevo-production.up.railway.app/',
