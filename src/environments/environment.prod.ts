da398a10dd2a93a44ff1527495cf1092dd05dd64
EduarVicen <123512047+EduarVicen@users.noreply.github.com>
2023-04-18 18:18:53 -0600

    ya esta 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
 export const environment = {
-  production: true
+   production: true,
+  //My RAILWAY 
+   
+  API_URI: 'https://nuevo-production.up.railway.app/',
+    //API_URI: 'http://localhost:4000/',
+  
+    
+  /**VARIABLES PARA LA GALERÍA */
+  Dish_image : "",
+  Socket_state : 0,
+  User_state : 1,
+
+  /**AQUI SE MANEJAN 3 POSIBLES ESTADOS 
+   * -1 PARA TODAS LAS CATEGORÍAS
+   *  0 PARA PRODUCTOS SIN CATEGORÍAS
+   *  1 EN ADELANTE UNA CATEGORÍA ESPECIFICA
+  */
+ 
+  filter_var : -1,
+  
+  /** INDICA EL NUMERO DE LA PAGINA EN LA QUE NOS ENCONTRAMOS ACTUALMENTE */
+  pagevalue : 1,
+
+  /**VARIABLES PARA ORDENES */
+   orderdetail_id : 0
 };
