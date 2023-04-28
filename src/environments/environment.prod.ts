c3c545f3e138d05b2a69b8f36a52a802974da8d5
EduarVicen <123512047+EduarVicen@users.noreply.github.com>
2023-04-27 20:48:00 -0600

    de nuevo 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
    production: true,
   //My RAILWAY 
    
-  API_URI: 'https://nuevo-production.up.railway.app/',
+  API_URI: 'https://nuevo-production-983c.up.railway.app/',
     //API_URI: 'http://localhost:4000/',
   
     
